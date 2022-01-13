5411a2f45c11d2511aa6b95ab2bb3ef92f2fa6ac
Joe Freeman <joefreeman8@gmail.com>
2022-01-13 14:24:48 +0000

    heroku setup

--- index.js
+++ index.js
@@ -4,10 +4,12 @@ import logger from './lib/logger.js'
 import router from './config/router.js'
 import { port } from './config/environment.js'
 import errorHandler from './lib/errorHandler.js'
+import cors from 'cors'
 
 const app = express()
 
 app.use(express.json())
+app.use(cors())
 app.use('/', logger)
 app.use('/api', router)
 app.use(errorHandler)
