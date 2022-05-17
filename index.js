3df6fd12155f4d66d98280489cbdc9a30ebd9c0e
Joe Freeman <joefreeman8@gmail.com>
2022-05-17 14:34:59 +0100

    cors fix

--- index.js
+++ index.js
@@ -9,12 +9,7 @@ import cors from 'cors'
 const app = express()
 
 app.use(express.json())
-app.use(cors({
-  'origin': '*',
-  'methods': 'GET,HEAD,PUT,PATCH,POST,DELETE',
-  'preflightContinue': false,
-  'optionsSuccessStatus': 204,
-}))
+app.use(cors())
 app.use('/', logger)
 app.use('/api', router)
 app.use(errorHandler)
